33b06901ed6f116493042ddfdfa8ed3fe71fb267
Princeshaarma <121950747+Princeshaarma@users.noreply.github.com>
2025-07-17 00:39:32 +0530

    Update AboutUs.jsx

--- Frontend/src/components/about-us/AboutUs.jsx
+++ Frontend/src/components/about-us/AboutUs.jsx
@@ -8,7 +8,7 @@ const teamMembers = [
     { id: 1, name: 'Nakul ', git:"https://github.com/NakulKataria", Linkedin:"https://www.linkedin.com/in/nakulkataria05/", mail:"mailto:nakulkofficial@gmail.com"},
     { id: 2, name: 'Rishit', git:"https://github.com/RENCHIO24", Linkedin:"https://www.linkedin.com/in/rishit-rastogi-73b6a22a6", mail:"mailto:rishrishit@gmail.com"},
     { id: 3, name: 'Shubham', git:"https://github.com/onceagainarise", Linkedin:"https://www.linkedin.com/in/shubham-3793a9257/ ", mail:"mailto:mrflame5883@gmail.com"},
-    { id: 4, name: 'Vivek', git:"https://github.com/vivekkumar1522", Linkedin:"https://www.linkedin.com/in/vivek-kumar-a143232b5", mail:"mailto:unitevivek9999@gmail.com "},
+    { id: 4, name: 'Prince', git:"https://github.com/Princeshaarma", Linkedin:"https://www.linkedin.com/in/prince-sharma-71aa77269/", mail:"mailto:princysharma8700@gmail.com "},
   ];
 
 const AboutUs = () => {
